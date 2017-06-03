febe8ff023c4305619f3b82a82440fcabbc1986e
Marcelo Ferranti <mferranti@Mac-de-Marcelo.local>
2017-06-03 11:05:10 -0300

    Ejercicio 2 - no es muy elegante

--- tp2/tp2.pl
+++ tp2/tp2.pl
@@ -7,11 +7,45 @@ herramienta(encendedor, 5).
 
 composicion(binaria(X,Y),P,5):-
   herramienta(X, PX), herramienta(Y,PY), X\= encendedor, P is 2*PX + PY.
-composicion(jerarquica(X,Y),P,2):-
+composicion(jerarquica(X,Y),P,4):-
   herramienta(X, PX), herramienta(Y,PY), P is PX * PY.
 composicion(jerarquica(X,Y),P,C):-
-  herramienta(X, PX), composicion(Y,PY,CY)), P is PX * PY, C is 2 * (1 + CX).
+  herramienta(X, PX),
+  composicion(Y,PY,CY),
+  P is PX * PY,
+  C is 2 * (1 + CY).
 composicion(jerarquica(X,Y),P,C):-
-  composicion(X, PX, CX), herramienta(Y,PY), P is PX * PY, C is 2 * (1 + CX).
+  composicion(X, PX, CX),
+  herramienta(Y,PY),
+  P is PX * PY,
+  C is 2 * (1 + CX).
 composicion(jerarquica(X,Y),P,C):-
-  composicion(X,P1,C1), composicion(Y,P2,C2), P is P1*P2, C is 2 * (C1 + C2).
+  composicion(X,P1,C1),
+  composicion(Y,P2,C2),
+  P is P1*P2,
+  C is 2 * (C1 + C2).
+
+%configuracion(+M, ?Conf, ?P, ?C)
+configuracion(M, Conf, P, C):-
+  permutations(M,NPERM),
+  member(N, NPERM),
+  herramientasDeConf(Conf, N),
+  composicion(Conf, P, C).
+
+
+herramientasDeConf(binaria(X,Y), [X,Y]):-
+  composicion(binaria(X,Y),_,_).
+
+herramientasDeConf(jerarquica(X,Y), [X,Y]):-
+  herramienta(X,_),
+  herramienta(Y,_).
+
+herramientasDeConf(jerarquica(X,Y), [X|HS]):-
+  herramienta(X,_),
+  herramientasDeConf(Y,HS).
+
+herramientasDeConf(jerarquica(X,Y), [Y|HS]):-
+  herramienta(Y,_),
+  herramientasDeConf(X,HS).
+
+permutations(X,L):-setof(A, permutation(X, A), L).
