0ba1645627b652c05a8707920125fe586bd144f9
Marcelo Ferranti <mferranti@Mac-de-Marcelo.local>
2017-07-20 02:08:29 -0300

    RTP2 para entregar

--- tp2/tp2.pl
+++ tp2/tp2.pl
@@ -4,33 +4,14 @@
 % Marcelo Ferranti 629/06
 % Natasha Martinelli 617/08
 
-% ####################################
-% Mochileando con herramientas
-% ####################################
-
 %%% Herramientas basicas
 herramienta(rayo, 10).
 herramienta(volatilizador, 40).
 herramienta(encendedor, 5).
 
-
-
 %%% Ejercicio 1
 % Definimos jerarquica y su composicion
 % composicion(+Composicion, ?Potencial, ?Costo)
-
-
-%binaria(X,Y):- herramienta(X,_), herramienta(Y,_). 
-%jerarquica(X,Y):- herramienta(X,_), herramienta(Y,_).
-%----------------------------------------anterior
-%composicion(binaria(X,Y),P,5):-herramienta(X, PX), herramienta(Y,PY), P is 2*PX + PY.
-%composicion(jerarquica(X,Y),P,4):-herramienta(X, PX), herramienta(Y,PY), P is PX * PY.
-%composicion(jerarquica(X,Y),P,C):-herramienta(X, PX),composicion(Y,PY,CY),P is PX * PY,C is 2 * (1 + CY).
-%composicion(jerarquica(X,Y),P,C):-composicion(X, PX, CX),herramienta(Y,PY),P is PX * PY,C is 2 * (1 + CX).
-%composicion(jerarquica(X,Y),P,C):-composicion(X,P1,C1),composicion(Y,P2,C2),P is P1*P2,C is 2 * (C1 + C2).
-%----------------------------------------anterior
-
-%composicion(jerarquia(jerarquia(binaria(rayo,volatilizador), rayo), rayo), X,Y).
 
 composicion(M, P, C) :- not(herramienta(M, _)), calcular(M, P, C).
 
@@ -45,14 +26,12 @@ calcular(jerarquica(X, Y), P, C) :- calcular(X, PX, CX), calcular(Y, PY, CY), P 
 
 configuracion(M, Conf, P, C):-permutations(M,NPERM),member(N, NPERM),herramientasDeConf(Conf, N),composicion(Conf, P, C).
 
-%falta corregirlo
 herramientasDeConf(binaria(X,Y), [X,Y]):-composicion(binaria(X,Y),_,_).
 herramientasDeConf(jerarquica(X,Y), [X,Y]):-herramienta(X,_),herramienta(Y,_).
 herramientasDeConf(jerarquica(X,Y), [X|HS]):-herramienta(X,_),herramientasDeConf(Y,HS).
 herramientasDeConf(jerarquica(X,Y), [Y|HS]):-herramienta(Y,_),herramientasDeConf(X,HS).
 
 permutations(X,L):-setof(A, permutation(X, A), L).
-
 
 %%% Ejercicio 3
 % Definimos masPoderosa, que dadas dos mochilas nos retorna true si existe un potencial en M1 tal que sea mayor a todos
@@ -65,68 +44,51 @@ maxPotencial(M, P):-findall(PConf, configuracion(M,_,PConf, _), L),max_member(P,
 
 
 %%% Ejercicio 4
-% Definimos mejor, que dadas dos mochilas nos retorna true si existe un potencial en M1 tal que sea mayor o igual a alguna
-%  configuraciones y tenga menor costo de M2
-%mejor(M1,M2)
+% Definimos mejor, que dadas dos mochilas nos retorna true si para toda configuracion de M2
+% existe una configuracion en M1 tal que el potencial sea mayor o igual que el de M2 y el costo sea menor.
+% mejor(+M1,+M2)
+% mejor(M1, M2) :- Para todo Conf2, Existe Conf1, P1 >= P2 y C1 < C2.
+% mejor(M1, M2) :- not ((Existe Conf2, para todo Conf1, P1 < P2 y C1 => C2)).
+% mejor(M1, M2) :- not ((Existe Conf2, not ((Existe Conf1, P1 >= P2 y C1 < C2)) )).
 
-%----------------------------------------anterior
-%mejor(M1,M2):- not(not(siempreHayMejor(M1,M2))).
+mejor(M1, M2) :- not((
+  configuracion(M2,_,P2, C2),
+  not((existeMejor(M1,P2,C2)))
+)).
 
-%siempreHayMejor(M1,M2):-configuracion(M2,_,PM2, CM2),existeMejor(M1,PM2,CM2).
+existeMejor(M1, P2, C2) :-
+  configuracion(M1, _, P1, C1),
+  P1 >= P2,
+  C1 < C2,
+  !.
 
-%existeMejor(M1,PM2,CM2):-configuracion(M1,_,PM1, CM1),PM1 >= PM2,CM1 < CM2,!.
-%----------------------------------------anterior
-
-mejor(M1, M2) :- not((configuracion(M2, _, PM2, CM2), not(existeMejor(M1, PM2, CM2)))).
-
-existeMejor(M1, PM2, CM2) :- configuracion(M1, _, PM1, CM1), PM1 >= PM2, CM1 < CM2, !.
 
 %%% Ejercicio 5
 % Definimos usar que a partir de una mochila M y una lista Ps de potenciales,
-%genera una lista Cs de herramientas compuestas que satisfacen los potenciales solicitados
-%usar(+M1,+Ps,?Cs,?M2)
+% genera una lista Cs de herramientas compuestas que satisfacen los potenciales solicitados
+% usar(+M1,+Ps,?Cs,?M2)
 
-%----------------------------------------anterior
-%usar(_,[],[],_).
-%usar(M1,[P|PS],[C|CS],M2):-setof(X,sublist(X,M1), L),
-%							member(M,L),deleteSublist(M,M1,M1Unused),
-%							configuracion(M, C, Pot, _),
-%							Pot >= P,usar(M1Unused, PS, CS, M2).
-
-%sublist([], _).
-%sublist([X|Xs], [X|Ys]) :- sublist(Xs, Ys).
-%sublist(Xs, [_|Ys]) :- sublist(Xs, Ys).
-
-%deleteSublist([],L,L).
-%deleteSublist([X|S],L,T):- deleteSublist(S,L,T2), selectchk(X,T2,T).
-%----------------------------------------anterior
-
-%usar([rayo,rayo,volatilizador,rayo,encendedor],[30,80],C,[encendedor]).
-
-usar(M1, [], [], M1).
-usar(M1, [P|PS], [C|CS], M2) :- sublist(X, M1), 
-								configuracion(X, C, Pot, _), 
-								Pot >= P,
-								remover(M1, X, NewM1),
-								usar(NewM1, PS, CS, M2).
+usar(_,[],[],_).
+usar(M1,[P|PS],[C|CS],M2):-
+  setof(X,sublist(X,M1), L),
+  member(M,L),
+  deleteSublist(M,M1,M1Unused),
+  configuracion(M, C, Pot, _),
+  Pot >= P,usar(M1Unused, PS, CS, M2).
 
 sublist([], _).
 sublist([X|Xs], [X|Ys]) :- sublist(Xs, Ys).
 sublist(Xs, [_|Ys]) :- sublist(Xs, Ys).
 
-remover(XS, [], XS).
-remover([X|XS], [X|YS], M2) :- remover(XS, YS, M2).
-remover([X|XS], [Y|YS], [X|M2]) :- X \= Y, remover(XS, [Y|YS], M2).
-
-%preguntar como no retornar repetidos
+deleteSublist([],L,L).
+deleteSublist([X|S],L,T):- deleteSublist(S,L,T2), selectchk(X,T2,T).
 
 %%% Ejercicio 6
-% Definimos comprar que es verdadero cuando  M tiene como mucho maximo
-%C herramientas basicas y permite construir una herramienta con potencial mayor o igual a P 
+% Definimos comprar que es verdadero cuando  M tiene como mucho maximo C
+% herramientas basicas y permite construir una herramienta con potencial mayor o igual a P 
 
-
-%comprar(+P,+C,?M)
-comprar(P,C,M):-setof(X, solucionesComprar(P,C,X), L),list_to_set(L,S),member(M,S).
+% comprar(+P,+C,?M)
+comprar(P,C,M):- setof(X, solucionesComprar(P,C,X), L),list_to_set(L,S),member(M,S).
 
 solucionesComprar(P,C,M):-generarMochilas(C,L),member(M,L),configuracion(M,_,Pot,_),Pot >= P.
 
